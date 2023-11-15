9cfb2cc97c3baa1ad0ae3256fad090db5d4b0808
pdongge1 <56106658+pdongge1@users.noreply.github.com>
2023-11-15 17:15:10 +0100

    Mise à jour main.blocks, main.ts, test.ts

--- main.ts
+++ main.ts
@@ -8,13 +8,17 @@ namespace ESP8266Smarteo {
     function resetESP() {
         sendAT("AT+RESTORE", 1000) // restore to factory settings
         sendAT("AT+RST", 1000) // reset the module
-        sendAT("AT+CWMODE=1", 500) // set to station mode
-    }
-
-    function isConnectedWiFi() : boolean {
-        sendAT("AT+CWJAP?", 5000)
+        sendAT("AT",500)
         let response = serial.readString()
-        return response.includes("NO AP") == false
+        if (response.includes("OK")) {
+            basic.showIcon(IconNames.Yes)
+            basic.pause(5000)
+        }
+        else {
+            basic.showIcon(IconNames.No)
+            basic.pause(5000)
+        }
+        sendAT("AT+CWMODE=1", 500) // set to station mode
     }
     
     /**
@@ -25,17 +29,11 @@ namespace ESP8266Smarteo {
     //% rx.defl='SerialPin.P0'
     //% baudrate.defl='baudrate.BaudRate115200'
     export function initesp8266(tx : SerialPin, rx : SerialPin, baudrate : BaudRate) {
-        try {
             serial.redirect(tx, rx, BaudRate.BaudRate115200)
             basic.pause(100)
             serial.setTxBufferSize(128)
             serial.setRxBufferSize(128)
             resetESP()
-        }
-        catch(e) {
-            basic.showString("Erreur pins :" + e)
-        }
-            
     }
 
     /**
@@ -45,13 +43,15 @@ namespace ESP8266Smarteo {
     //% ssid.defl='Smarteo'
     //% password.defl='%Smarteo123'
     //% ip_address.defl='192.168.1.30'
-    export function connectToWifi(ssid : string, password : string, ip_address : string) : boolean {
+    export function connectToWifi(ssid : string, password : string, ip_address : string) {
         sendAT(`AT+CWJAP="${ssid}","${password}"`, 5000)
-        if(!isConnectedWiFi()) {
-            basic.showString("echec du connexion wifi")
-            return false
+        sendAT("AT+CWJAP?",5000)
+        let response = serial.readString()
+        if (response.includes("NO AP") == false) {
+            basic.showIcon(IconNames.Happy)
         }
-        sendAT(`AT+CIPSTA="${ip_address}"`, 1000)
-        return true
+        else {
+            basic.showIcon(IconNames.Angry)
+        }
     }
  }
